dfb10cf0ad6f57f30396203a6dcb89e2316b1bb8
savchenkovartyom <90179315+savchenkovartyom@users.noreply.github.com>
2021-09-06 11:02:52 +0200

    Delete LaboratoryWork_4 directory

--- LaboratoryWork_4/src/LaboratoryWork_4/LaboratoryWork_4.java
+++
@@ -1,8 +0,0 @@
-package LaboratoryWork_4;
-import LaboratoryWork_1.*;
-public class LaboratoryWork_4 {
-    public static void main(String[] args) {
-        LaboratoryWork_1_1 obj = new LaboratoryWork_1_1();
-        obj.Go();
-    }
-}
